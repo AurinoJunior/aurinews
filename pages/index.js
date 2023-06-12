f5f1a858653d72fa772f325bc4ef62cbeb247fc4
Aurino Geraldo <aurinoaj@gmail.com>
2023-06-12 10:27:42 -0300

    First deploy

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Feliz dia dos namorados amor 😁 obrigado por sempre me apoiar, e cada dia que passa eu sei que é vc que eu quero comigo todos os dias. 💕</h1>
+}
+
+export default Home
