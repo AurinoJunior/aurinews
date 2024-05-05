1d9b617a7b6e6db15cd068b6998793454f7b272c
Aurino Junior <aurinoaj@gmail.com>
2024-05-05 04:39:38 +0000

    feat: create test to opened connections

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -2,16 +2,19 @@ import database from "infra/database"
 
 export default async function (request, response) {
   const updateAt = new Date().toISOString();
-  const dbVersion = await database.query("SELECT version();");
-  const dbMaxConnection = await database.query("SHOW max_connections;");
-  
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbOpenedConnections = 
+    await database
+      .query("SELECT count(*)::int FROM pg_stat_activity WHERE state = 'active';");
 
   return response.status(200).json({
     updated_at: updateAt,
     dependencies: {
       database: {
-        version: dbVersion.rows[0].version,
-        max_connections: parseInt(dbMaxConnection.rows[0].max_connections),
+        version: dbVersion.rows[0].server_version,
+        max_connections: parseInt(dbMaxConnections.rows[0].max_connections),
+        opened_connections: dbOpenedConnections.rows[0].count,
       }
     },
   })
--- tests/integration/v1/status.test.js
+++ tests/integration/v1/status.test.js
@@ -5,7 +5,8 @@ describe("/api/v1/status", () => {
     const body = await response.json()
 
     expect(response.status).toBe(200)
-    expect(body.dependencies.database.version).toContain("PostgreSQL 16.0")
+    expect(body.dependencies.database.version).toBe("16.0")
     expect(body.dependencies.database.max_connections).toBe(100)
+    expect(body.dependencies.database.opened_connections).toBe(1)
   })
 })
