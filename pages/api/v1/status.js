6bf1ab2b0062bccdc639725a88f190826e3bf217
Aurino Junior <aurinoaj@gmail.com>
2024-05-05 05:11:37 +0000

    fix: multi connections errors and fix sql injection

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,12 +1,17 @@
 import database from "infra/database"
 
 export default async function (request, response) {
+  const dbName = process.env.POSTGRES_DB
+
   const updateAt = new Date().toISOString();
   const dbVersion = await database.query("SHOW server_version;");
   const dbMaxConnections = await database.query("SHOW max_connections;");
   const dbOpenedConnections = 
     await database
-      .query("SELECT count(*)::int FROM pg_stat_activity WHERE state = 'active';");
+      .query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+        values: [dbName]
+      });
 
   return response.status(200).json({
     updated_at: updateAt,
