279fc736eab6a2617ff313f270d83e3168d3df1f
Aurino Junior <aurinoaj@gmail.com>
2024-05-05 04:17:09 +0000

    feat: create new tests to status endpoint

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,6 +1,18 @@
 import database from "infra/database"
 
 export default async function (request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;")
-  return response.status(200).json({result: result.rows.sum})
+  const updateAt = new Date().toISOString();
+  const dbVersion = await database.query("SELECT version();");
+  const dbMaxConnection = await database.query("SHOW max_connections;");
+  
+
+  return response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersion.rows[0].version,
+        max_connections: parseInt(dbMaxConnection.rows[0].max_connections),
+      }
+    },
+  })
 }
--- tests/integration/v1/status.test.js
+++ tests/integration/v1/status.test.js
@@ -1,4 +1,11 @@
-test("/api/v1/status", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
-  expect(response.status).toBe(200)
+
+describe("/api/v1/status", () => {
+  it("should return 200 when get /status", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status")
+    const body = await response.json()
+
+    expect(response.status).toBe(200)
+    expect(body.dependencies.database.version).toContain("PostgreSQL 16.0")
+    expect(body.dependencies.database.max_connections).toBe(100)
+  })
 })
