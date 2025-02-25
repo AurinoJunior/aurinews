ffbfd928da1317c29191e9cff872f297009dc30a
Aurino Geraldo <aurinoaj@gmail.com>
2025-02-24 22:05:37 -0300

    fix: change join to resolve on the dir migration

--- pages/api/v1/migrations.js
+++ pages/api/v1/migrations.js
@@ -1,5 +1,5 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import { resolve } from "node:path";
 import database from "infra/database.js";
 
 export default async function (request, response) {
@@ -13,7 +13,7 @@ export default async function (request, response) {
     client = await database.getNewClient();
     const migrationOptions = {
       dbClient: client,
-      dir: join("infra", "migrations"),
+      dir: resolve("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
